af447634a3bf8fed69586f4c9b1c3f4c758b3c19
Matías Boldrini <50892896+MatiasBoldrini@users.noreply.github.com>
2023-04-09 21:20:48 -0300

    removed time library

--- Practico_1/main.py
+++ Practico_1/main.py
@@ -3,9 +3,6 @@ Usando una matriz de transición. Para implementar la matriz de transición en P
 [[2,1,1,'','',''],
 [2,'','','','' ,'']
 ['acepta', 'acepta', 'acepta', 'acepta', 'acepta','acepta']]"""
-
-import time
-
 matriz = [
     ['2', '1', '1', ' ', ' ',' '],
     ['2', ' ', ' ', ' ', ' ',' '],
@@ -32,17 +29,17 @@ def automata(cadena):
             else: 
                 raise ValueError
         except (IndexError, ValueError) as e:
-            return False, f'{cadena} no es un número identificable'
+            return f'{cadena} no es un número identificable'
     
         #print(f'Estado actual: {estado_actual}\n Columna: {columna}\n Caracter{cadena[pos]}')
         estado_actual = matriz[int(estado_actual)][columna]
         if estado_actual == ' ':
-            return False, f'{cadena} no es un número identificable'
+            return f'{cadena} no es un número identificable'
         if estado_actual == '8':
-            return True, f'{cadena} es un número'
+            return f'{cadena} es un número'
 
 
 if __name__ == '__main__':
     numeros_test = ['1ab2$','12.e4$','a1$','1e12e0$','++$','e12$','120379$', '+120379$', '-120379$', '120379e14$', '120379e-14$', '120379e+14$', '12.0379$', '1203.79e-14$']
     for number in numeros_test:
-        print(automata(number)[1])
+        print(automata(number))
