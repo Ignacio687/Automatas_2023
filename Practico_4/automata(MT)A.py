56fdb78dbb711596042a510f9deff48fc9fa2291
Ignacio687 <ignadavidchaves@gmail.com>
2023-06-04 14:03:41 -0300

    TP4 listo

--- Practico_4/automata(MT)A.py
+++ Practico_4/automata(MT)A.py
@@ -3,7 +3,7 @@
 class InitStateError(Exception):
     pass
 
-class CharacterNotPresentError(InitStateError):
+class CharacterNotPresentError(Exception):
     pass
 
 class MaquinaDeTuring():
@@ -39,10 +39,10 @@ class MaquinaDeTuring():
             new_values = self.changeSt(state, stringList[index])
             state = new_values[0]
             stringList[index] = new_values[1]
+            if stringList[index] == self.blank and str(state) in self.endSts:
+                return [True, self.statesLog]
             index += 1 if new_values[2] in ["r","R"] else -1
             if (index+1 > len(stringList) or index < 0) and str(state) in self.endSts:
-                return [True, self.statesLog]
-            elif stringList[index] == self.blank and str(state) in self.endSts:
                 return [True, self.statesLog]
         return [False, self.statesLog]
 
@@ -56,11 +56,11 @@ class MaquinaDeTuring():
         self.statesLog["Edo. Actual"].append(st)
         self.statesLog["Caracter"].append(char)
         try:
-            new_values = self.transitions[(st, char)]
+            new_values = self.transitions[(st, element[0])]
             self.statesLog["Edo. Siguiente"].append(new_values[0])
             return new_values
         except KeyError:
-            raise CharacterNotPresentError(f"'{char}' no esta presente en ninguna transición", self.statesLog)
+            raise CharacterNotPresentError(f"'{char}' no esta presente en ninguna transición con el estado '{st}'", self.statesLog)
         
 
 class MaquinaDeTuringApp():
@@ -71,26 +71,26 @@ class MaquinaDeTuringApp():
         print("\n\nEsta aplicacion interpreta maquinas de turing y las ejecuta para cumplir su proposito.\n")
         app = ""
         while type(app) == str:
-            user_input = input("Si quiere probar una de las maquinas ya cargadas oprima 'enter', de lo contrario, si desea introducir su propia maquina ingrese 'nueva':  ")
+            user_input = input("Si quiere probar una de las maquinas ya cargadas oprima 'enter', de lo contrario, si desea introducir su propia maquina ingrese 'nueva':  ").replace(" ", "")
             if user_input in ["nueva", "Nueva", "NUEVA"]:
                 app = self.newTM()
             else:
                 app = self.selectTM()
         while True:
-            user_input = input("Ingrese una cadena a evaluar y presione 'enter':  ")
-            user_input2 = input("Si desea comenzar a analizar la cadena desde un caracter en particular ingrese el numero, sino presione 'enter' y comenzara por la izquierda:  ")
+            user_input = input("Ingrese una cadena a evaluar y presione 'enter':  ").replace(" ", "")
+            user_input2 = input("Si desea comenzar a analizar la cadena desde un caracter en particular ingrese el numero, sino presione 'enter' y comenzara por la izquierda:  ").replace(" ", "")
             print("\n"+user_input+"\n")
             try:
                 if user_input2 != "" and user_input2.isdigit():
+                    user_input2 = 0 if int(user_input2) > len(user_input) else user_input2
                     self.printer(app.analyse(user_input, int(user_input2)))
                 else: 
                     self.printer(app.analyse(user_input))
-            except (ValueError or CharacterNotPresentError) as e:
-                self.printer(e.args)
-            user_input = input("Si desea ingresar una cadena nueva oprima el 'enter', sino ingrese 'no' para salir:  ")
+            except (ValueError, CharacterNotPresentError) as e:
+                self.printer(e)
+            user_input = input("Si desea ingresar una cadena nueva oprima el 'enter', sino ingrese 'no' para salir:  ").replace(" ", "")
             if user_input in ["no", "No", "NO"]:
                 break
-
 
     def newTM(self):
         estado_inicial = input("Ingrese el estado inicial y presione 'enter':  ").replace(" ", "")
@@ -99,29 +99,32 @@ class MaquinaDeTuringApp():
         while True:
             print("Siguiente estado")
             user_input = input("Ingrese un estado y presione 'enter' (si ya no hay mas simplemente presione 'enter sin ingresar nada'):  ").replace(" ", "")
-            estados_finales.append(user_input)
             if user_input == "":
                 print(f"\n\n{estados_finales}\n\n")
                 break
+            estados_finales.append(user_input)
         caracter_vacio = input("Si utiliza el caracter vacio en algunas de las trasiciones ingreselo a continuacion y presione 'enter', si no lo utiliza no ingrese nada y simplemente presione 'enter':  ").replace(" ", "")
         caracteres = {}
         print("Ahora ingrese el o los caracteres uno a uno, al terminar de ingresar el ultimo caracter presione 'enter'")
         while True:
             print("Siguiente caracter")
             user_input = input("Ingrese un caracter y presione 'enter' (si ya no hay mas simplemente presione 'enter sin ingresar nada'):  ").replace(" ", "")
+            if user_input == "":
+                print(f"\n\n{caracteres}\n\n")
+                break
             user_input2 = input("Si el caracter era una representacion, por ejemplo: 'num': 1,2,3,4, ingrese los caracteres que representa seguidos sin separadores, por ejemplo: 1234. Si el caracter no es una representacion simplemente presione 'enter' sin ingresar nada:  ").replace(" ", "")
             if user_input2 == "":
                 caracteres[user_input] = user_input
             else: caracteres[user_input] = user_input2
-            if user_input == "":
-                print(f"\n\n{caracteres}\n\n")
-                break
         transiciones = {}
         print("Ahora ingrese las transiciones una a una, al terminar de ingresar la ultima transicion presione 'enter'")
         while True:
             print("Siguiente Transicion")
             print("La sintaxis de las transiciones es la siguiente: ('q1','a'): ('q2','b','r o l')")
             user_input = input("Ingrese el estado inicial y presione 'enter' (si ya no hay mas simplemente presione 'enter sin ingresar nada'):  ").replace(" ", "")
+            if user_input == "":
+                print(transiciones)
+                break
             print("\n('q1',  )\n~~~~~~^^~")
             user_input2 = input("Ingrese el caracter que acompaña al estado y presione 'enter':  ").replace(" ", "")
             print("\n('q1','a'): (  ,)\n~~~~~~~~~~~~~^^~")
@@ -131,9 +134,6 @@ class MaquinaDeTuringApp():
             print("\n('q1','a'): ('q2','b',  )\n~~~~~~~~~~~~~~~~~~~~~~^^~")
             user_input5 = input("Ahora ingrese 'l' si es un desplazamiento a izquierda o 'r' si es a derecha y presione 'enter':  ").replace(" ", "")
             transiciones[(user_input, user_input2)] = (user_input3, user_input4, user_input5)
-            if user_input == "":
-                print(caracteres)
-                break
         try:
             app = MaquinaDeTuring(estado_inicial, estados_finales, caracter_vacio, caracteres, transiciones)
             return app
@@ -147,23 +147,29 @@ class MaquinaDeTuringApp():
         print("1) Maquina A, reconoce el lenguaje representado por esta expresion regular: 'x ( x | y ) *' (Automata ejercicio 2)a)")
         print("2) Maquina B, reconoce el lenguaje representado por esta expresion regular: '(C | AC) *' (Automata ejercicio 2)b)")
         print("3) Maquina C, reconoce el lenguaje representado por esta expresion regular: '(b | (b* a) *) a' (Automata ejercicio 2)c)")
+        print("4) Maquina D, reconoce el lenguaje de las operaciones aritmeticas (+,-,*,/) entre digitos")
         while True:
             try:
                 user_input = input("Ingrese el numero correspondiente a la maquina que desea utilizar:  ").replace(" ", "")
                 if user_input == "1":
-                    transiciones = {("q1", "x"): ("q2", "x", "R"), ("q2", "x"): ("q2", "x", "R"), ("q2", "y"): ("q2", "y", "R"), ("q2", "B"): ("q3", "B", "R")}
+                    transiciones = {("q1", "x"): ("q2", "x", "R"), ("q2", "x"): ("q2", "x", "R"), ("q2", "y"): ("q2", "y", "R"), ("q2", "B"): ("q3", "B", "L")}
                     app = MaquinaDeTuring("q1", ["q3"], "B", {"x":"x", "y":"y", "B":"B"}, transiciones)
                     return app
                 elif user_input == "2":
-                    transiciones = {("q1", "A"): ("q2", "A", "R"), ("q1", "C"): ("q1", "C", "R"), ("q1", "B"): ("q3", "B", "R"), ("q2", "C"): ("q1", "C", "R")}
+                    transiciones = {("q1", "A"): ("q2", "A", "R"), ("q1", "C"): ("q1", "C", "R"), ("q1", "B"): ("q3", "B", "L"), ("q2", "C"): ("q1", "C", "R")}
                     app = MaquinaDeTuring("q1", ["q3"], "B", {"A":"A", "C":"C", "B":"B"}, transiciones)
                     return app
                 elif user_input == "3":
                     transiciones = {("q1", "a"): ("q5", "a", "R"),("q1", "b"): ("q2", "b", "R"),("q2", "a"): ("q5", "a", "R"),("q2", "b"): ("q3", "b", "R"),
                                     ("q3", "a"): ("q4", "a", "R"),("q3", "b"): ("q3", "b", "R"), ("q4", "a"): ("q5", "a", "R"),("q4", "b"): ("q3", "b", "R"),
-                                    ("q5", "a"): ("q5", "a", "R"),("q5", "b"): ("q3", "b", "R"),("q5", "B"): ("q6", "B", "R")
+                                    ("q5", "a"): ("q5", "a", "R"),("q5", "b"): ("q3", "b", "R"),("q5", "B"): ("q6", "B", "L")
                                     }
                     app = MaquinaDeTuring("q1", ["q6"], "B", {"a":"a", "b":"b", "B":"B"}, transiciones)
+                    return app
+                elif user_input == "4":
+                    transiciones = {('q0', 'digito'): ('q1', 'digito', 'r'), ('q1', 'digito'): ('q1', 'digito', 'r'), ('q1', 'operador'): ('q2', 'operador', 'r'), 
+                                    ('q2', 'digito'): ('q3', 'digito', 'r'), ('q3', 'operador'): ('q2', 'operador', 'r'), ('q3', 'digito'): ('q3', 'digito', 'r')}
+                    app = MaquinaDeTuring("q1", ["q3"], None, {'operador': '+-*/', 'digito': '0123456789'}, transiciones)
                     return app
             except InitStateError as e:
                 print("\n\n"+"ERROR!!\n"+e.args[0]+"\n\n")
@@ -171,13 +177,15 @@ class MaquinaDeTuringApp():
                 return app
 
     def printer(self, data):
-        if type(data) == Exception:
-            exc_msg = data[0]
+        exc_msg = ""
+        if type(data) in [ValueError, CharacterNotPresentError]:
+            data = list(data.args)
+            exc_msg = "ERROR!!\n"+data[0]+"\n\n"
             data[0] = False
         print("+--------------+---------+-----------+---------------+")
         print("""|  Edo. Actual |Caracter |  Simbolo  |Edo. Siguiente |""")
         print("+--------------+---------+-----------+---------------+")
-        for fase in range(0, len(data[1]["Edo. Actual"])):
+        for fase in range(0, len(data[1]["Edo. Siguiente"])):
             print("|     ",data[1]["Edo. Actual"][fase],"     |   ",data[1]["Caracter"][fase],"   |    ", data[1]["Simbolo"][fase],"    |     ",data[1]["Edo. Siguiente"][fase],"      |")
             print("+--------------+---------+-----------+---------------+")
         if data[0]:
@@ -186,8 +194,8 @@ class MaquinaDeTuringApp():
         else:
             print("""|              Cadena No Valida :(                   |""")
             print("+--------------+---------+-----------+---------------+\n\n")
-        if type(data) == Exception:
-            print("ERROR!!\n"+exc_msg)
+        print(exc_msg, end="")
+
 
 if __name__ == "__main__":
     app = MaquinaDeTuringApp()
