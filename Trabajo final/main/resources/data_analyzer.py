17964412fe7d86df35a1ca8d0a3eb84bf3f53563
Matías Boldrini <50892896+MatiasBoldrini@users.noreply.github.com>
2023-07-02 18:36:35 -0300

    working, cool mac printing and ui

--- Trabajo final/main/resources/data_analyzer.py
+++ Trabajo final/main/resources/data_analyzer.py
@@ -3,17 +3,40 @@ from datetime import datetime, timedelta, date
 from collections import Counter
 class IncorrectFileFormat(Exception):
     pass
-
 class DataAnalyzer():
     def __init__(self, filePath: str) -> None:
         """Class capable of validating a specific format csv file and 
         returning filtered data.\n
         filePath: Absolute path of the file"""
         self.filePath = filePath
-        startDate = datetime.strptime("2003-01-01", "%Y-%m-%d")
-        endDate = datetime.strptime("2023-06-30", "%Y-%m-%d")
-        self.filterUsers(startDate=startDate, endDate=endDate)
-
+        self.user_data = {}
+        #startDate = datetime.strptime("2003-01-01", "%Y-%m-%d")
+        #endDate = datetime.strptime("2023-06-30", "%Y-%m-%d")
+        #self.filterUsers(startDate=startDate, endDate=endDate)
+    def __str__(self):
+        user_data = self.user_data
+        final_str = ''
+        for key in user_data.keys():
+            final_str += f"\n\n{'-'*35} MOSTRANDO DATOS DESDE {self.fecha_inicio} HASTA {self.fecha_fin} {'-'*35}\n"
+            final_str += f"\n{key}:"
+            final_str += f"\n\tUsername: {user_data[key]['username']} "
+            mac_dict = user_data[key]['mac_cliente']
+            connection_count = Counter(mac for mac in mac_dict for _ in mac_dict[mac])
+            most_connected_mac = connection_count.most_common(1)[0][0]
+            total_hours = {mac: sum(int(hour) for hour in mac_dict[mac]) for mac in mac_dict}
+            mac_with_longest_duration = max(total_hours, key=total_hours.get)
+            final_str += f"\n\tMost used mac adress: {most_connected_mac}"
+            final_str += f"\n\tMost time connected mac {mac_with_longest_duration}"
+            mac_keys = [i for i in mac_dict.keys()]
+            # Dividir la lista en sublistas de 4 elementos
+            sublists = [mac_keys[i:i+4] for i in range(0, len(mac_keys), 4)]
+            # Unir cada sublista utilizando tabulaciones
+            formatted_keys = '\n\t\t'.join(['\t'.join(sublist) for sublist in sublists])
+            final_str += f'\n\tTotal Mac adress:\n\t\t{formatted_keys}'
+            final_str += f"\n\tConexiones:"
+            for i in user_data[key]['conexiones']:
+                final_str += f"\n\t\t{i}"
+        return final_str
     def validate(self) -> dict[int, str|int]:
         """Method capable of validating the file format and all data within the lines. 
         Returns a dictionary with all the found errors '{line_index:[errors_list]}'."""
@@ -27,9 +50,9 @@ class DataAnalyzer():
         pass
 
     def filterUsers(self, 
-                    filter: bool = True, 
                     startDate = None, 
-                    endDate = None
+                    endDate = None,
+                    filter: bool = True, 
                     ):
         """Method designed to filter the users that have accessed the system on Non-working days. 
         Returns a dictionary {user_name: [total_session_time, most_used_MAC_NWD, most_used_MAC_WD, 
@@ -42,14 +65,16 @@ class DataAnalyzer():
         Session_Time,Input_Octects,Output_Octects,MAC_AP,MAC_Cliente,Razon_de_Terminación_de_Sesión,,
         603877,5AA0184E-000001CA,d6104707df0cd315,invitado-deca,192.168.247.11,Wireless-802.11,2019-02-07,19:46:08,2019-03-13,11:27:57,25,39517,505219,DC-9F-DB-12-F3-EA:HCDD,DC-BF-E9-1A-B5-D0,User-Request,,
         """
-        fecha_inicio = date(2020, 3, 22) ## Argumentos
-        fecha_fin = date(2020, 3, 22)
+        fecha_inicio = startDate
+        fecha_fin = endDate
         if fecha_inicio == None:
             fecha_inicio = date(2000, 3, 27) # Fecha antigua para incluir todos los resultados
         if fecha_fin == None:
             fecha_fin = date.today()
         if fecha_fin < fecha_inicio:
             fecha_fin, fecha_inicio = fecha_inicio, fecha_fin
+        self.fecha_inicio = fecha_inicio
+        self.fecha_fin = fecha_fin
 
 
         def is_weekend(fecha_inicio, fecha_fin):
@@ -78,23 +103,6 @@ class DataAnalyzer():
                     fecha_actual += delta
                 return False
 
-        def print_data():
-            for key in user_data.keys():
-                print(f"\n{'-'*35} MOSTRANDO DATOS DESDE {fecha_inicio} HASTA {fecha_fin} {'-'*35}\n")
-                print(f"{key}:")
-                print(f"\tUsername: {user_data[key]['username']} ")
-                mac_dict = user_data[key]['mac_cliente']
-                connection_count = Counter(mac for mac in mac_dict for _ in mac_dict[mac])
-                most_connected_mac = connection_count.most_common(1)[0][0]
-                total_hours = {mac: sum(int(hour) for hour in mac_dict[mac]) for mac in mac_dict}
-                mac_with_longest_duration = max(total_hours, key=total_hours.get)
-                print(f"\tMost used mac adress: {most_connected_mac}")
-                print(f"\tMost time connected mac {mac_with_longest_duration}")
-                print(mac_dict)
-                print(f"\tConexiones:")
-                for i in user_data[key]['conexiones']:
-                    print(f"\t\t{i}")
-                    
         user_data = {}
         with open(self.filePath) as csv_file:
             csv_reader = csv.DictReader(csv_file, delimiter=',')
@@ -106,7 +114,6 @@ class DataAnalyzer():
                 mac_cliente     = row['MAC_Cliente']
                 input_octets    = row['Input_Octects']
                 output_octects  = row['Output_Octects']
-
                 if inicio_conexion <= fecha_fin and fin_conexion >= fecha_inicio and is_weekend(max(inicio_conexion, fecha_inicio), min(fin_conexion, fecha_fin)): 
                     if not user_data.get(username):
                         user_data[username] = {'username': username, 'mac_cliente': {}, 'conexiones': [] } 
@@ -116,11 +123,10 @@ class DataAnalyzer():
                         user_data[username]['mac_cliente'][mac_cliente] = [row['Session_Time']]
                     io_octets = f"input_octets: {row['Input_Octects']:<10} | output_octects: {row['Output_Octects']:<10}"
                     user_data[username]['conexiones'].append(f"{row['Inicio_de_Conexión_Dia']} -> {row['FIN_de_Conexión_Dia']} => Total time: {row['Session_Time']+' hs':<8} | {io_octets}")
-        print_data()
+            self.user_data = user_data
 
+        #print_data()
 
-
-                    
 if __name__ == '__main__':
     d = DataAnalyzer('/Users/matiasboldrini/Facu/Automatas_2023/Trabajo final/main/data/test_files/test_mati2.csv')
     # d = DataAnalyzer('/Users/matiasboldrini/Facu/Automatas_2023/Trabajo final/archivo.csv')
