4feca632acceeedb61b4bd01ffa1c7bc756732f5
Matías Boldrini <50892896+MatiasBoldrini@users.noreply.github.com>
2023-07-02 01:35:08 -0300

    mac adress hours and most connected

--- Trabajo final/main/resources/data_analyzer.py
+++ Trabajo final/main/resources/data_analyzer.py
@@ -1,6 +1,6 @@
 import pathlib,csv, json
 from datetime import datetime, timedelta, date
-
+from collections import Counter
 class IncorrectFileFormat(Exception):
     pass
 
@@ -42,8 +42,8 @@ class DataAnalyzer():
         Session_Time,Input_Octects,Output_Octects,MAC_AP,MAC_Cliente,Razon_de_Terminación_de_Sesión,,
         603877,5AA0184E-000001CA,d6104707df0cd315,invitado-deca,192.168.247.11,Wireless-802.11,2019-02-07,19:46:08,2019-03-13,11:27:57,25,39517,505219,DC-9F-DB-12-F3-EA:HCDD,DC-BF-E9-1A-B5-D0,User-Request,,
         """
-        fecha_inicio = date(2019, 3, 26) ## Argumentos
-        fecha_fin = date(2019, 3, 28)
+        fecha_inicio = date(2020, 3, 22) ## Argumentos
+        fecha_fin = date(2020, 3, 22)
         if fecha_inicio == None:
             fecha_inicio = date(2000, 3, 27) # Fecha antigua para incluir todos los resultados
         if fecha_fin == None:
@@ -51,33 +51,46 @@ class DataAnalyzer():
         if fecha_fin < fecha_inicio:
             fecha_fin, fecha_inicio = fecha_inicio, fecha_fin
 
-        def is_holyday(date):
-            #main/data/2019.json
-            with open(f"main/data/{date.year}.json") as json_file:
-                data = json.load(json_file)
-                month = date.month-1
-                day = my_date.strftime("%d")
-                if day in [i for i in data[month]]:
-                    return True
-        
+
         def is_weekend(fecha_inicio, fecha_fin):
-            delta = timedelta(days=1)
-            if (fecha_fin - fecha_inicio).days > 5:
-                return True
-            fecha_actual = fecha_inicio
-            while fecha_actual <= fecha_fin:
-                dia_semana = fecha_actual.weekday()
-                if dia_semana in [5, 6] :  # 5 representa sábado y 6 representa or is_holyday(fecha_actual)
-                    return True
-                fecha_actual += delta
-            return False
+            """Method designed to validate if a date range is has a weekend or holyday.
+            Args:
+                fecha_inicio (datetime.date): start date
+                fecha_fin (datetime.date): end date
+            Returns:
+                bool: returns True if there is at least one weekend or holyday within the dates range.
+            """
+            with open(f"main/data/{fecha_inicio.year}.json") as json_file:
+                fecha_actual = fecha_inicio
+                data = json.load(json_file)
+                delta = timedelta(days=1)
+                fecha_actual = fecha_inicio
+                while fecha_actual <= fecha_fin:
+                    month = fecha_actual.month-1
+                    day = fecha_actual.strftime("%d")
+                    dia_semana = fecha_actual.weekday()
+                    if fecha_actual.year != fecha_inicio.year:
+                        # Json files are separated by years
+                        # Open next year json and run the same procedure.
+                        return is_weekend(fecha_actual, fecha_fin)
+                    if dia_semana in [5, 6] or day in [i for i in data[month].keys()]: 
+                        return True
+                    fecha_actual += delta
+                return False
 
         def print_data():
             for key in user_data.keys():
-                print(f"\n{'-'*30} MOSTRANDO DATOS DESDE {fecha_inicio} HASTA {fecha_fin} {'-'*30}\n")
+                print(f"\n{'-'*35} MOSTRANDO DATOS DESDE {fecha_inicio} HASTA {fecha_fin} {'-'*35}\n")
                 print(f"{key}:")
                 print(f"\tUsername: {user_data[key]['username']} ")
-                print(f"\tMac adress: {user_data[key]['mac_cliente']}")
+                mac_dict = user_data[key]['mac_cliente']
+                connection_count = Counter(mac for mac in mac_dict for _ in mac_dict[mac])
+                most_connected_mac = connection_count.most_common(1)[0][0]
+                total_hours = {mac: sum(int(hour) for hour in mac_dict[mac]) for mac in mac_dict}
+                mac_with_longest_duration = max(total_hours, key=total_hours.get)
+                print(f"\tMost used mac adress: {most_connected_mac}")
+                print(f"\tMost time connected mac {mac_with_longest_duration}")
+                print(mac_dict)
                 print(f"\tConexiones:")
                 for i in user_data[key]['conexiones']:
                     print(f"\t\t{i}")
@@ -88,41 +101,26 @@ class DataAnalyzer():
             for row in csv_reader:
                 inicio_conexion = datetime.strptime(row['Inicio_de_Conexión_Dia'], "%Y-%m-%d").date()
                 fin_conexion = datetime.strptime(row['FIN_de_Conexión_Dia'], "%Y-%m-%d").date()
-                session_time = row['Session_Time']
-                username = row['Usuario']
-                mac_cliente = row['MAC_Cliente']
-                input_octets = row['Input_Octects']
-                output_octects = row['Output_Octects']
-                if not(inicio_conexion > fecha_fin or fin_conexion < fecha_inicio) and is_weekend(inicio_conexion, fin_conexion): 
+                session_time    = row['Session_Time']
+                username        = row['Usuario']
+                mac_cliente     = row['MAC_Cliente']
+                input_octets    = row['Input_Octects']
+                output_octects  = row['Output_Octects']
+
+                if inicio_conexion <= fecha_fin and fin_conexion >= fecha_inicio and is_weekend(max(inicio_conexion, fecha_inicio), min(fin_conexion, fecha_fin)): 
                     if not user_data.get(username):
-                        user_data[username] = {'username': username, 'mac_cliente': [mac_cliente], 'conexiones': [] } 
-                #if user_data.get(username) : # and len(user_data.get(username)['conexiones'])
-                    if mac_cliente not in user_data[username]['mac_cliente']:
-                        user_data[username]['mac_cliente'].append(mac_cliente)
-                    user_data[username]['conexiones'].append(f"{row['Inicio_de_Conexión_Dia']} -> {row['FIN_de_Conexión_Dia']} => Total time: {row['Session_Time']+' hs':<8} | input_octets: {row['Input_Octects']:<10} | output_octects: {row['Output_Octects']:<10}")
+                        user_data[username] = {'username': username, 'mac_cliente': {}, 'conexiones': [] } 
+                    if user_data[username]['mac_cliente'].get(mac_cliente):
+                        user_data[username]['mac_cliente'][mac_cliente].append(row['Session_Time'])
+                    else:
+                        user_data[username]['mac_cliente'][mac_cliente] = [row['Session_Time']]
+                    io_octets = f"input_octets: {row['Input_Octects']:<10} | output_octects: {row['Output_Octects']:<10}"
+                    user_data[username]['conexiones'].append(f"{row['Inicio_de_Conexión_Dia']} -> {row['FIN_de_Conexión_Dia']} => Total time: {row['Session_Time']+' hs':<8} | {io_octets}")
         print_data()
 
 
-def is_holyday(fecha_inicio, fecha_fin):
-    with open(f"main/data/{fecha_inicio.year}.json") as json_file:
-        delta = timedelta(days=1)
-            # if (fecha_fin - fecha_inicio).days > 5:
-            #     return True
-        fecha_actual = fecha_inicio
-        data = json.load(json_file)
-        month = fecha_actual.month-1
-        day = fecha_actual.strftime("%d")
-        print(data[month])
-        while fecha_actual <= fecha_fin:
-            if day in [i for i in data[month]]:
-                print(f"Fecha {fecha_actual} es feriado")
-            print(f"Fecha {fecha_actual} no es feriado")
-            fecha_actual += delta
+
                     
 if __name__ == '__main__':
-    #my_date = date(2020, 3, 2)
-    # fecha_inicio = date(2020, 3, 1) ## Argumentos
-    # fecha_fin = date(2020, 4, 1)
-    # print(is_holyday(fecha_inicio, fecha_fin))
-    d = DataAnalyzer('/Users/matiasboldrini/Facu/Automatas_2023/Trabajo final/main/data/test_files/test_mati.csv')
+    d = DataAnalyzer('/Users/matiasboldrini/Facu/Automatas_2023/Trabajo final/main/data/test_files/test_mati2.csv')
     # d = DataAnalyzer('/Users/matiasboldrini/Facu/Automatas_2023/Trabajo final/archivo.csv')
