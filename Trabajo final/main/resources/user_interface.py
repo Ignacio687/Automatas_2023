454bd3539133583d64d674d8a7b7df98f355c51a
Ignacio687 <ignadavidchaves@gmail.com>
2023-07-03 17:42:43 -0300

    pinto xd

--- Trabajo final/main/resources/user_interface.py
+++ Trabajo final/main/resources/user_interface.py
@@ -1,5 +1,5 @@
 from data_analyzer import DataAnalyzer
-from datetime import time, datetime
+from datetime import time, datetime, date
 
 class UserInterface():
     def __init__(self) -> None:
@@ -11,7 +11,7 @@ class UserInterface():
         while True:
             try:
                 #path = input('Ingrese el path del archivo a analizar (.csv): ')
-                path = '/Users/matiasboldrini/Facu/Automatas_2023/Trabajo final/archivo.csv'
+                path = '/home/ignaciochaves/code/python/Automatas_2023/Trabajo final/data.csv'
                 data = DataAnalyzer(path)
                 open(path)
                 break
@@ -37,7 +37,26 @@ class UserInterface():
                     break
                 print("Se debe ingresar la fecha en formate '%Y-%m-%d'. Intente de nuevo")
         print("Buscando entre los usuarios que se han conectado días feriados y fines de semana..")
-        data.filterUsers(startDate, endDate)
-        print(f"\n{data.__str__()}")
+        userData, startDate, endDate = data.filterUsers(startDate, endDate)
+        print(userData)
+        #self.strFormat(userData, startDate, endDate)
+
+
+    def strFormat(self, userData: dict[str, dict[str, dict[str, int]]], startDate: str|date, endDate: str|date):
+        final_str = ''
+        for username in userData.keys():
+            final_str += f"\n{'-'*55} MOSTRANDO DATOS DESDE {startDate} HASTA {endDate} {'-'*55}\n"
+            final_str += f"Username: {username} "
+            final_str += f"\n\tDireccion MAC mas usada: {userData[username][most_used_mac]}"
+            final_str += f"\n\tDireccion MAC mas tiempo conectada: {userData[username][mac_most_time]}\n"
+            final_str += f"\n\tConexiones:"
+            
+            for mac in userData[username].keys():               
+                final_str += f""" \n\t{mac}: Session time: {datetime.strptime(str(userData[username][mac]['Session_time']),"%H:%M:%S"):<10} 
+                                | Input Bytes: {userData[username][mac]['Input_Bytes']:<10} | Output Bytes: {userData[username][mac]['Output_Bytes']:<10} 
+                                | Session Count: {userData[username][mac]['Session_count']} """
+            
+        return final_str
+
 if __name__ == '__main__':
     UserInterface().run()
