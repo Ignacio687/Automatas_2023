ff2a8c26abe67c794fe686c4e0b494e38d7c6320
Ignacio687 <ignadavidchaves@gmail.com>
2023-07-04 01:42:28 -0300

    all done

--- Trabajo final/main/test/test_user_interface.py
+++
@@ -1,9 +0,0 @@
-import unittest, pathlib
-from main.resources import UserInterface
-
-class UserInterfaceTestCase(unittest.TestCase):
-    def setUp(self) -> None:
-        self.app = UserInterface()
-
-    def test_1(self) -> None:
-        pass
